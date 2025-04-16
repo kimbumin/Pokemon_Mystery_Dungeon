4b455f8c5dd58efbc9478d872c5d211506b5c53c
eviloliver <evil_oliver@kakao.com>
2025-04-17 00:43:45 +0900

    WIP: Player and Movement
    - push를 깜빡한걸로 추정되지만 구상만 한 뼈대
    - 방향 enum 클래스를 쌍으로 바꿔줄 필요가 있어보인다.
    - 이동을 시간동안 목적지를 향해 이동하는 느낌으로 만들어야 한다
    - 애니메이션마다 재생시간을 따로 두어야 한다

---
+++ Pokemon_Mystery_Dungeon/Pokemon_Mystery_Dungeon/PokemonMovement.cpp
@@ -0,0 +1,13 @@
+#include "PokemonMovement.h"
+
+void PokemonMovement::Move(Direction direction)
+{
+}
+
+void PokemonMovement::Look(Direction direction)
+{
+}
+
+void PokemonMovement::Update()
+{
+}
---
+++ Pokemon_Mystery_Dungeon/Pokemon_Mystery_Dungeon/PokemonPlayer.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "PokemonBase.h"
+
+class PokemonPlayer : public PokemonBase
+{
+private:
+
+protected:
+
+public:
+
+};
+
