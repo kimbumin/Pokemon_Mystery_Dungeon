afb7274d32ba31025468d55aa721394efa79fcbe
BOOM <bumin43@gmail.com>
2025-04-17 11:22:27 +0900

    Update: IItem
    아이템 인터페이스 추상 함수들 추가

--- Pokemon_Mystery_Dungeon/Pokemon_Mystery_Dungeon/IItem.h
+++ Pokemon_Mystery_Dungeon/Pokemon_Mystery_Dungeon/IItem.h
@@ -1,1 +1,13 @@
+// IItem.h
 #pragma once
+#include "GameObject.h"
+
+class IItem : public GameObject {
+public:
+	virtual ~IItem() {}
+	virtual void Use() = 0;
+	virtual int GetX() const = 0;
+	virtual int GetY() const = 0;
+	virtual void SetPosition(int x, int y) = 0;
+};
+
