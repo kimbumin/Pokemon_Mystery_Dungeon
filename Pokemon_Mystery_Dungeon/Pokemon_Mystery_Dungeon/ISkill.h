5196bfe9df568fb37fbba53e122f99f8dbac6111
BOOM <bumin43@gmail.com>
2025-04-22 14:14:17 +0900

    Refactor: ISkill
    스킬 인터페이스와 스킬베이스 합치고, 스킬Use기능 수정

--- Pokemon_Mystery_Dungeon/Pokemon_Mystery_Dungeon/ISkill.h
+++ Pokemon_Mystery_Dungeon/Pokemon_Mystery_Dungeon/ISkill.h
@@ -1,24 +1,45 @@
 #pragma once
-#include "config.h"
 #include "GameObject.h"
-#include "PlayerBumin.h"
-#include "EnemyBumin.h"
+
+struct SkillData {
+    int number;
+    string name;
+    string element;
+    string type;
+    string animAction;
+    int power;
+    int accuracy;
+    int pp;
+};
+
+class PokemonBase;
 
 class ISkill : public GameObject {
+protected:
+    SkillData data;
+    FPOINT pos;
+    int direction;
+    bool isActive;
+
 public:
-    virtual HRESULT Init() = 0;
-    virtual void Release() = 0;
-    virtual void Update() = 0;
-    virtual void Render(HDC hdc) = 0;
-    virtual void Use(PlayerBumin* user, EnemyBumin* target) = 0;
+    //ISkill() = default;
+    //ISkill(const SkillData& skillData) : data(skillData) {}
+
+    virtual HRESULT Init() const = 0;
+    virtual void Release() const = 0;
+    virtual void Update() const = 0;
+    virtual void Render(HDC hdc) const = 0;
+    virtual void Use(PokemonBase* owner) const = 0;
     virtual shared_ptr<ISkill> Clone() const = 0;
 
+    // getter ÇÔ¼öµé
+    inline string GetName() const { return data.name; }
+    inline string GetElement() const { return data.element; }
+    inline string GetSkillType() const { return data.type; }
+    inline string GetAnimAction() const { return data.animAction; }
+    inline int GetPower() const { return data.power; }
+    inline int GetAccuracy() const { return data.accuracy; }
+    inline const SkillData& GetSkillData() const { return data; }
 
-    virtual string GetName() const = 0;
-    virtual string GetElement() const = 0;
-    virtual string GetType() const = 0;
-    virtual int GetPower() const = 0;
-    virtual int GetAccuracy() const = 0;
-
-    virtual ~ISkill() = default;
+    //virtual ~ISkill() = default;
 };
