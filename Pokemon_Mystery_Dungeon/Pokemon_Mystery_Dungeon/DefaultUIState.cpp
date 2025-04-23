56c4405f286b93387021f5576aa16eae193fc21a
0175077 <0175077@naver.com>
2025-04-23 11:04:41 +0900

    Update : SkillState

--- Pokemon_Mystery_Dungeon/Pokemon_Mystery_Dungeon/DefaultUIState.cpp
+++ Pokemon_Mystery_Dungeon/Pokemon_Mystery_Dungeon/DefaultUIState.cpp
@@ -1,6 +1,7 @@
 #include "DefaultUIState.h"
 
 #include "ImageGDIPlusManager.h"
+#include "SkillUIState.h"
 #include "UIElementImage.h"
 #include "UIElementText.h"
 #include "UIManager.h"
@@ -19,7 +20,6 @@ HRESULT DefaultUIState::Init()
     auto CursorImage =
         manager.AddImage("Cursor", L"Image/UIImage/DefaultUIState/Cursor.png");
 
-
     // UI 엘리먼트 생성
     OtherInterfaceInfoBox = new UIElementImage();
     PokemonInfoBox = new UIElementImage();
@@ -31,20 +31,18 @@ HRESULT DefaultUIState::Init()
     MapNameText = new UIElementText();
     PokemonNameText = new UIElementText();
     PokemonLevelText = new UIElementText();
-    PokemonHPText = new UIElementText();    
+    PokemonHPText = new UIElementText();
 
     OtherInterfaceInfoBox->SetImage(OtherInterfaceInfoBoxImage);
     PokemonInfoBox->SetImage(PokemonInfoBoxImage);
     MapInfoBox->SetImage(MapInfoBoxImage);
     Cursor->SetImage(CursorImage);
 
-    
-
     /*--------------- 텍스트 설정 ---------------*/
-    //otherBox
+    // otherBox
     SkillText->SetText(L"Skill");
     SkillText->SetFont(L"DungGeunMo", 18);
-    SkillText->SetTextLine(5.0f);  
+    SkillText->SetTextLine(5.0f);
     SkillText->SetLocalPos(fontPosX, OffsetY[0]);
     SkillText->SetParent(OtherInterfaceInfoBox);
 
@@ -64,7 +62,7 @@ HRESULT DefaultUIState::Init()
     MapNameText->SetText(L"Map Name");
     MapNameText->SetFont(L"Arial", 18);
     MapNameText->SetTextLine(5.0f);
-    MapNameText->SetLocalPos(50, MapInfoBox->GetImageHeight()/2 -10);
+    MapNameText->SetLocalPos(50, MapInfoBox->GetImageHeight() / 2 - 10);
     MapNameText->SetParent(MapInfoBox);
 
     // pokemonInfoBox
@@ -78,7 +76,8 @@ HRESULT DefaultUIState::Init()
     PokemonLevelText->SetText(L"Level 1");
     PokemonLevelText->SetFont(L"Arial", 18);
     PokemonLevelText->SetTextLine(5.0f);
-    PokemonLevelText->SetLocalPos(50, PokemonInfoBox->GetImageHeight() / 2 + 20);
+    PokemonLevelText->SetLocalPos(50,
+                                  PokemonInfoBox->GetImageHeight() / 2 + 20);
     PokemonLevelText->SetParent(PokemonInfoBox);
 
     PokemonHPText->SetText(L"HP 100 / 100");
@@ -161,8 +160,6 @@ void DefaultUIState::Update()
     {
         UIManager::GetInstance()->ChangeState("IdleUI");
     }
-
-
 }
 
 void DefaultUIState::Render(HDC hdc)
