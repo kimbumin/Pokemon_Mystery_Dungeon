2c8f850b8a14bcf3abb0d0279614352dafe82cf9
BOOM <bumin43@gmail.com>
2025-04-23 10:49:49 +0900

    Update: StoneShower
    스톤샤워 스킬 기능 추가및 이미지,csv이름 수정

--- Pokemon_Mystery_Dungeon/Pokemon_Mystery_Dungeon/StoneShowerSkill.cpp
+++ Pokemon_Mystery_Dungeon/Pokemon_Mystery_Dungeon/StoneShowerSkill.cpp
@@ -1,1 +1,74 @@
 #include "StoneShowerSkill.h"
+
+#include "PokemonBase.h"
+
+StoneShowerSkill::StoneShowerSkill(const SkillData& skillData)
+{
+    data = skillData;
+}
+
+HRESULT StoneShowerSkill::Init()
+{
+    pos = {0, 0};
+    isActive = false;
+    frameCount = 0;
+    elapsedTime = 0.0f;
+    image = ImageManager::GetInstance()->AddImage(
+        "StoneShower", TEXT("Image/SkillImage/StoneShower.bmp"), 312, 54, 8,
+        1, true, RGB(255, 0, 255));
+
+    return S_OK;
+}
+
+void StoneShowerSkill::Release()
+{
+}
+
+void StoneShowerSkill::Update()
+{
+    if (isActive)
+    {
+        elapsedTime += TimerManager::GetInstance()->GetDeltaTime();  // 누적
+
+        if (elapsedTime >= 0.02f)  // 0.2초마다 실행
+        {
+            frameCount++;
+            elapsedTime = 0.0f;  // 다시 0으로 초기화
+        }
+
+        if (frameCount >= image->GetMaxFrameX())
+        {
+            isActive = false;
+            frameCount = 0;
+        }
+    }
+}
+
+void StoneShowerSkill::Render(HDC hdc)
+{
+    if (isActive && image)
+    {
+        int frameX = frameCount;
+        if (frameX >= image->GetMaxFrameX())
+            frameX = image->GetMaxFrameX() - 1;
+
+        image->FrameRender(hdc, pos.x, pos.y, frameX, 0);
+    }
+}
+
+void StoneShowerSkill::Use(PokemonBase* owner)
+{
+    direction = static_cast<int>(owner->GetDirection());
+    pos = owner->GetPos();
+    auto dirIndex = static_cast<size_t>(direction);
+
+    pos.x += directionOffsets[dirIndex].first * 24;
+    pos.y += directionOffsets[dirIndex].second * 24;
+
+    isActive = true;
+}
+
+shared_ptr<ISkill> StoneShowerSkill::Clone()
+{
+    return make_shared<StoneShowerSkill>(*this);  // 스킬을 복제하여 반환
+}
--- Pokemon_Mystery_Dungeon/Pokemon_Mystery_Dungeon/StoneShowerSkill.h
+++ Pokemon_Mystery_Dungeon/Pokemon_Mystery_Dungeon/StoneShowerSkill.h
@@ -1,4 +1,27 @@
 #pragma once
-class StoneShowerSkill
+#include "ISkill.h"
+// clang-format off
+#include "config.h"
+// clang-format on
+#include "Image.h"
+
+class Image;
+class PokemonBase;
+class StoneShowerSkill : public ISkill
 {
+private:
+    Image* image;
+    float elapsedTime;
+    int frameCount;
+
+public:
+    StoneShowerSkill() = default;
+    StoneShowerSkill(const SkillData& skillData);
+
+    HRESULT Init() override;
+    void Release() override;
+    void Update() override;
+    void Render(HDC hdc) override;
+    void Use(PokemonBase* owner) override;
+    shared_ptr<ISkill> Clone() override;
 };
